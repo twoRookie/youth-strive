3948b13a399754e1834cc9bf8c7ae30543cc2c14
hepengc <hepengc@worldunion.com.cn>
2018-10-19 17:55:26 +0800

    去掉rr标志

--- youth-admin/src/main/java/io/youth/modules/job/task/TestTask.java
+++ youth-admin/src/main/java/io/youth/modules/job/task/TestTask.java
@@ -1,7 +1,7 @@
 package io.youth.modules.job.task;
 
 /**
- * Copyright 2018 人人开源 http://www.renren.io
+ *
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
